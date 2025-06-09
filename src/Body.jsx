9a163e37ec6405eba6c9af756d16128a39226e23
Harsh <harshmann8008@gmail.com>
2025-06-09 17:59:09 +0530

    setup react router, add routing and layout components, implement Navbar, Body, Footer, Login, and Profile components

---
+++ src/Body.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+function Body() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <NavBar />
+            <main className="flex-grow">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Body
